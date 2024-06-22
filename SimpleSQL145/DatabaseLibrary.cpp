b1f5d5bba954c5ab4e7868d68b11c33de4d8afac
veselinoWiktor <vveselinov11@gmail.com>
2024-06-22 10:08:03 +0300

    implement loading of database data

--- SimpleSQL145/DatabaseLibrary.cpp
+++ SimpleSQL145/DatabaseLibrary.cpp
@@ -1,5 +1,16 @@
 #include "DatabaseLibrary.h"
 
+void Database::loadDatabase(std::ifstream& ifs)
+{
+	String currTblPath;
+	while (!ifs.eof())
+	{
+		ifs >> currTblPath;
+		Table currTbl((FilePath)currTblPath);
+		tables.pushBack(currTbl);
+	}
+}
+
 Database::Database(const FilePath& relativePath)
 {
 	if (relativePath.getExtension() != ".db")
@@ -7,9 +18,18 @@ Database::Database(const FilePath& relativePath)
 		throw std::logic_error("Database::Database(); File was not with extension .db");
 	}
 
+	String path = (relativePath.getName() + relativePath.getExtension());
+	std::ifstream ifs(path.c_str());
+	if (!ifs.is_open())
+	{
+		throw std::logic_error("Database::Database(); Database file was already open!");
+	}
+
+	loadDatabase(ifs);
+	ifs.close();
 }
 
 SQLResponse Database::executeQuery(const String& query)
 {
-	return SQLResponse();
+	SQLCommand* command = 
 }
--- SimpleSQL145/DatabaseLibrary.h
+++ SimpleSQL145/DatabaseLibrary.h
@@ -1,16 +1,20 @@
 #pragma once
 
+#include "Vector.hpp"
+
+#include "SQLCommand.h"
+#include "SQLCommandFactory.h"
+#include "SQLResponse.h"
 #include "FilePath.h"
 #include "Table.h"
-#include "SQLResponse.h"
 
 class Database
 {
 private:
 	FilePath pathToDB = (String)"No path"; // TODO: might want to fix this cast
-	Table* tables;
-	size_t tablesCount;
+	Vector<Table> tables;
 
+	void loadDatabase(std::ifstream& ifs);
 public:
 	Database(const FilePath& relativePath);
 	SQLResponse executeQuery(const String& query);
